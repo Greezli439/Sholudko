dd6cc7a83450bd57e63bf88e704c09394838f7bb
mykhailo <mykhailo.sholudko@gmail.com>
2023-01-08 11:49:56 +0200

    split func. main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from pathlib import Path
-
 """
 This program collects and processes information from
 personnel log files and provides a list with ID,
 name, date and time of entry or exit to the facility.
 """
+
+
+from pathlib import Path
+
 
 PATH = '/home/mykhailo/Hillel/Final_Project/Sholudko/venv/'
 CREW_FILENAME = 'crew.txt'
@@ -35,24 +37,30 @@ def main(path: str) -> dict:
              facility.
     """
     crew, enter, exit = read_files(path)
-    d_crew, res, d_res = {}, {}, {}
-    for i in crew:
-        d_crew[i.split(' |')[0]] = i.split(' |')[1].replace('\n', '')
-    for k, name in d_crew.items():
-        p_res = {}
+    d_crew, res = {}, {}
+
+
+    def make_dict(k: str) -> dict:
+        p = {}
         for i in enter:
             k2, dt = i.split(' |')[0], i.split(' |')[1]
             if k2 == k:
                 for j in exit:
+                    time = []
                     k3, dt3 = j.split(' |')[0], j.split(' |')[1]
-                    if k3 == k2 and j.split(' |')[1].split()[0] \
-                                 == i.split(' |')[1].split()[0]:
-                        p_res.setdefault(dt.split()[0], [])\
-                            .append([i.split(' |')[1].split()[1],
-                            j.split(' |')[1].split()[1]])
+                    if k3 == k2 and dt3.split()[0] == (dts0 := dt.split()[0]):
+                        time.append(dt.split()[1])
+                        time.append(dt3.split()[1])
+                        p.setdefault(dts0, []).append([time[0], time[1]])
                         exit.remove(j)
                         break
-        res[k] = {'name': name.strip(), 'visits': p_res}
+        return p
+
+
+    for st in crew:
+        d_crew[st.split(' |')[0]] = st.split(' |')[1].replace('\n', '')
+    for k, name in d_crew.items():
+        res[k] = {'name': name.strip(), 'visits': make_dict(k)}
     return res
 
 
